06fc54120ee7d94a1ec5c441de65b71673c6e0fe
Kangzingu <kanrhaehfdl1@naver.com>
2022-10-28 14:03:47 +0900

    1문제 품
    가장 긴 증가하는 부분 수열4

---
+++ CodingTest/가장 긴 증가하는 부분 수열4.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int numofNums;
+	cin >> numofNums;
+
+	int* nums = new int[numofNums];
+	int** dp = new int* [numofNums];
+	for (int i = 0; i < numofNums; i++)
+	{
+		cin >> nums[i];
+		dp[i] = new int[2];
+		dp[i][0] = 1;
+		dp[i][1] = -1;
+	}
+
+	int maxVal = 0;
+	int maxValIdx;
+	for (int i = 0; i < numofNums; i++)
+	{
+		for (int j = i; j < numofNums; j++)
+		{
+			if (dp[i][0] + 1 > dp[j][0] && nums[i] < nums[j])
+			{
+				dp[j][0] = dp[i][0] + 1;
+				dp[j][1] = i;
+
+			}
+
+			if (dp[j][0] > maxVal)
+			{
+				maxVal = dp[j][0];
+				maxValIdx = j;
+			}
+		}
+	}
+
+	vector<int> result;
+
+	cout << maxVal << endl;
+	int val = nums[maxValIdx];
+	int idx = dp[maxValIdx][1];
+	result.push_back(val);
+	while (true)
+	{
+		if (idx == -1)
+			break;
+
+		result.push_back(nums[idx]);
+		idx = dp[idx][1];
+	}
+
+	for (int i = result.size() - 1; i >= 0; i--)
+	{
+		cout << result[i] << " ";
+	}
+
+	return 0;
+}
