d3395c5655c670c04d29497b483af57974e69edc
Kangzingu <kanrhaehfdl1@naver.com>
2022-11-02 21:28:21 +0900

    1문제 품
    타일 장식물

--- CodingTest/점프 점프.cpp
+++ CodingTest/점프 점프.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int numofNums;
-	cin >> numofNums;
-
-	int* nums = new int[numofNums];
-	int* dp = new int[numofNums];
-	for (int i = 0; i < numofNums; i++)
-	{
-		cin >> nums[i];
-		dp[i] = 1001;
-	}
-	dp[0] = 0;
-	for (int i = 0; i < numofNums; i++)
-	{
-		for (int j = i + 1; j <= i + nums[i]; j++)
-		{
-			if (j >= numofNums)
-				continue;
-
-			if (dp[i] + 1 < dp[j])
-				dp[j] = dp[i] + 1;
-		}
-	}
-
-	if (dp[numofNums - 1] == 1001)
-		cout << -1;
-	else
-		cout << dp[numofNums - 1];
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int numofNums;
+//	cin >> numofNums;
+//
+//	int* nums = new int[numofNums];
+//	int* dp = new int[numofNums];
+//	for (int i = 0; i < numofNums; i++)
+//	{
+//		cin >> nums[i];
+//		dp[i] = 1001;
+//	}
+//	dp[0] = 0;
+//	for (int i = 0; i < numofNums; i++)
+//	{
+//		for (int j = i + 1; j <= i + nums[i]; j++)
+//		{
+//			if (j >= numofNums)
+//				continue;
+//
+//			if (dp[i] + 1 < dp[j])
+//				dp[j] = dp[i] + 1;
+//		}
+//	}
+//
+//	if (dp[numofNums - 1] == 1001)
+//		cout << -1;
+//	else
+//		cout << dp[numofNums - 1];
+//
+//	return 0;
+//}
