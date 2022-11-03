61c59ba5f10f9186d3e1eb29bc9a3fe840723143
Kangzingu <kanrhaehfdl1@naver.com>
2022-11-03 23:27:23 +0900

    1문제 품
    피보나치 비스무리한 수열

--- CodingTest/타일 장식물.cpp
+++ CodingTest/타일 장식물.cpp
@@ -1,52 +1,52 @@
-#include <iostream>
-using namespace std;
-
-int targetNum;
-unsigned long long dp[81];
-
-unsigned long long DP(int num)
-{
-	if (dp[num] != 0)
-	{
-		return dp[num];
-	}
-
-	return dp[num] = DP(num - 1) + DP(num - 2);
-}
-
-int main()
-{
-	cin >> targetNum;
-	
-	for (int i = 0; i < 81; i++)
-	{
-		dp[i] = 0;
-	}
-	dp[1] = 1;
-	dp[2] = 1;
-	
-	unsigned long long result;
-	result = DP(targetNum) * 3 +
-		DP(targetNum - 1) * 2 +
-		DP(targetNum - 2) * 2 +
-		DP(targetNum - 3);
-
-	if (targetNum == 1)
-	{
-		cout << 4;
-	}
-	else if (targetNum == 2)
-	{
-		cout << 6;
-	}
-	else if (targetNum == 3)
-	{
-		cout << 10;
-	}
-	else
-	{	
-		cout << result;
-	}
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int targetNum;
+//unsigned long long dp[81];
+//
+//unsigned long long DP(int num)
+//{
+//	if (dp[num] != 0)
+//	{
+//		return dp[num];
+//	}
+//
+//	return dp[num] = DP(num - 1) + DP(num - 2);
+//}
+//
+//int main()
+//{
+//	cin >> targetNum;
+//	
+//	for (int i = 0; i < 81; i++)
+//	{
+//		dp[i] = 0;
+//	}
+//	dp[1] = 1;
+//	dp[2] = 1;
+//	
+//	unsigned long long result;
+//	result = DP(targetNum) * 3 +
+//		DP(targetNum - 1) * 2 +
+//		DP(targetNum - 2) * 2 +
+//		DP(targetNum - 3);
+//
+//	if (targetNum == 1)
+//	{
+//		cout << 4;
+//	}
+//	else if (targetNum == 2)
+//	{
+//		cout << 6;
+//	}
+//	else if (targetNum == 3)
+//	{
+//		cout << 10;
+//	}
+//	else
+//	{	
+//		cout << result;
+//	}
+//
+//	return 0;
+//}
---
+++ CodingTest/피보나치 비스무리한 수열.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+unsigned long long dp[117];
+
+unsigned long long DP(int num)
+{
+	if (dp[num] != 0)
+	{
+		return dp[num];
+	}
+
+	return dp[num] = DP(num - 1) + DP(num - 3);
+}
+int main()
+{
+	int targetNum;
+	cin >> targetNum;
+	
+	for (int i = 0; i < 117; i++)
+	{
+		dp[i] = 0;
+	}
+	dp[1] = 1;
+	dp[2] = 1;
+	dp[3] = 1;
+
+	cout << DP(targetNum);
+
+	return 0;
+}
