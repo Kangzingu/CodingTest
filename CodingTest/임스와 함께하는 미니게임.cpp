7b23a096dd1e135d48ef88b57c59da4eb1d90f3b
Kangzingu <kanrhaehfdl1@naver.com>
2022-11-18 15:06:11 +0900

    1문제 품
    회전 초밥

--- CodingTest/임스와 함께하는 미니게임.cpp
+++ CodingTest/임스와 함께하는 미니게임.cpp
@@ -1,37 +1,37 @@
-#include <iostream>
-#include <unordered_map>
-using namespace std;
-
-int main()
-{
-	int numofPlayer;
-	char gameType;
-	cin >> numofPlayer >> gameType;
-
-	unordered_map<string, bool> players;
-	string playerName;
-	for (int i = 0; i < numofPlayer; i++)
-	{
-		cin >> playerName;
-		players[playerName] = true;
-	}
-
-	int numofRealPlayer = players.size();
-	int gameCount;
-	switch (gameType)
-	{
-		case 'Y':
-			gameCount = numofRealPlayer;
-			break;
-		case 'F':
-			gameCount = numofRealPlayer / 2;
-			break;
-		case 'O':
-			gameCount = numofRealPlayer / 3;
-			break;
-	}
-
-	cout << gameCount;
-
-	return 0;
-}
+//#include <iostream>
+//#include <unordered_map>
+//using namespace std;
+//
+//int main()
+//{
+//	int numofPlayer;
+//	char gameType;
+//	cin >> numofPlayer >> gameType;
+//
+//	unordered_map<string, bool> players;
+//	string playerName;
+//	for (int i = 0; i < numofPlayer; i++)
+//	{
+//		cin >> playerName;
+//		players[playerName] = true;
+//	}
+//
+//	int numofRealPlayer = players.size();
+//	int gameCount;
+//	switch (gameType)
+//	{
+//		case 'Y':
+//			gameCount = numofRealPlayer;
+//			break;
+//		case 'F':
+//			gameCount = numofRealPlayer / 2;
+//			break;
+//		case 'O':
+//			gameCount = numofRealPlayer / 3;
+//			break;
+//	}
+//
+//	cout << gameCount;
+//
+//	return 0;
+//}
