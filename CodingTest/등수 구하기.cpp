cfe765765e52c89986d2e8d8412d70747bcd433e
Kangzingu <kanrhaehfdl1@naver.com>
2022-12-01 23:49:36 +0900

    1문제 품
    빗물

--- CodingTest/등수 구하기.cpp
+++ CodingTest/등수 구하기.cpp
@@ -1,54 +1,54 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	long long numofScore, newScore, sizeofRank;
-	cin >> numofScore >> newScore >> sizeofRank;
-
-	long long curRank = 0;
-	long long rankStack = 1;
-	long long prevInputScore = 2000000001;
-	long long curInputScore = 2000000001;
-	long long savedRank = 0;
-	for (int i = 0; i < numofScore; i++)
-	{
-		cin >> curInputScore;
-		
-		if (curInputScore == prevInputScore)
-			rankStack++;
-		else
-		{
-			curRank += rankStack;
-			rankStack = 1;
-		}
-
-		if (newScore > curInputScore)
-		{
-			if (savedRank == 0)
-				cout << curRank + rankStack - 1;
-			else
-				cout << savedRank;
-
-			return 0;
-		}
-		else if (newScore == curInputScore)
-			if (savedRank == 0)
-				savedRank = curRank + rankStack - 1;
-
-		if (i + 1 >= sizeofRank)
-		{
-			cout << -1;
-			return 0;
-		}
-
-		prevInputScore = curInputScore;
-	}
-
-	if (newScore < curInputScore)
-		cout << curRank + rankStack;
-	else if (newScore == curInputScore)
-		cout << savedRank;
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	long long numofScore, newScore, sizeofRank;
+//	cin >> numofScore >> newScore >> sizeofRank;
+//
+//	long long curRank = 0;
+//	long long rankStack = 1;
+//	long long prevInputScore = 2000000001;
+//	long long curInputScore = 2000000001;
+//	long long savedRank = 0;
+//	for (int i = 0; i < numofScore; i++)
+//	{
+//		cin >> curInputScore;
+//		
+//		if (curInputScore == prevInputScore)
+//			rankStack++;
+//		else
+//		{
+//			curRank += rankStack;
+//			rankStack = 1;
+//		}
+//
+//		if (newScore > curInputScore)
+//		{
+//			if (savedRank == 0)
+//				cout << curRank + rankStack - 1;
+//			else
+//				cout << savedRank;
+//
+//			return 0;
+//		}
+//		else if (newScore == curInputScore)
+//			if (savedRank == 0)
+//				savedRank = curRank + rankStack - 1;
+//
+//		if (i + 1 >= sizeofRank)
+//		{
+//			cout << -1;
+//			return 0;
+//		}
+//
+//		prevInputScore = curInputScore;
+//	}
+//
+//	if (newScore < curInputScore)
+//		cout << curRank + rankStack;
+//	else if (newScore == curInputScore)
+//		cout << savedRank;
+//
+//	return 0;
+//}
---
+++ CodingTest/빗물.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sizeofRow, sizeofCol;
+	cin >> sizeofRow >> sizeofCol;
+
+	int* heights = new int[sizeofCol];
+	for (int i = 0; i < sizeofCol; i++)
+		cin >> heights[i];
+
+	int rainAmount = 0;
+	int startCol;
+	for (int i = sizeofRow - 1; i >= 0; i--)
+	{
+		startCol = -1;
+		for (int j = 0; j < sizeofCol; j++)
+		{
+			if (i > heights[j] - 1)
+				continue;
+
+			if (startCol == -1)
+			{
+				startCol = j;
+				continue;
+			}
+
+			rainAmount += j - startCol - 1;
+			startCol = j;
+		}
+	}
+
+	cout << rainAmount;
+	
+	return 0;
+}
