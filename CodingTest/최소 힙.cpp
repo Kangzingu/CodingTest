3106bb7b1e4c34799f8ea39de3064f0c3368cb31
Kangzingu <kanrhaehfdl1@naver.com>
2022-12-09 15:55:16 +0900

    1문제 품
    비밀번호 발음하기

--- CodingTest/최소 힙.cpp
+++ CodingTest/최소 힙.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-#include <queue>
-using namespace std;
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int numofCommand;
-	cin >> numofCommand;
-
-	priority_queue<int, vector<int>, greater<int>> pq;
-	int curNum;
-	for (int i = 0; i < numofCommand; i++)
-	{
-		cin >> curNum;
-		if (curNum == 0)
-		{
-			if (pq.empty() == true)
-			{
-				cout << 0 << "\n";
-				continue;
-			}
-
-			cout << pq.top() << "\n";
-			pq.pop();
-			continue;
-		}
-
-		pq.push(curNum);
-	}
-
-	return 0;
-}
+//#include <iostream>
+//#include <queue>
+//using namespace std;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//
+//	int numofCommand;
+//	cin >> numofCommand;
+//
+//	priority_queue<int, vector<int>, greater<int>> pq;
+//	int curNum;
+//	for (int i = 0; i < numofCommand; i++)
+//	{
+//		cin >> curNum;
+//		if (curNum == 0)
+//		{
+//			if (pq.empty() == true)
+//			{
+//				cout << 0 << "\n";
+//				continue;
+//			}
+//
+//			cout << pq.top() << "\n";
+//			pq.pop();
+//			continue;
+//		}
+//
+//		pq.push(curNum);
+//	}
+//
+//	return 0;
+//}
