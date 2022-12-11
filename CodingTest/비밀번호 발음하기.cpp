0b5fa9db081425216e27d936ce8c690183252628
Kangzingu <kanrhaehfdl1@naver.com>
2022-12-11 20:37:54 +0900

    1문제 품
    삼각형과 세 변

--- CodingTest/비밀번호 발음하기.cpp
+++ CodingTest/비밀번호 발음하기.cpp
@@ -1,84 +1,84 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-bool isVowel(char inputChar)
-{
-	if (inputChar == 'a' ||
-		inputChar == 'e' ||
-		inputChar == 'i' ||
-		inputChar == 'o' ||
-		inputChar == 'u')
-		return true;
-
-	return false;
-}
-
-int main()
-{
-	string inputString;
-	while (true)
-	{
-		cin >> inputString;
-		if (inputString == "end")
-			break;
-
-		bool isVowelExist = false;
-		bool isContCountOver3 = false;
-		bool isSameCharCountOver2 = false;
-		
-		char curChar;
-		char prevChar = '-';
-		int contVowelCount = 0;
-		int contConsonantCount = 0;
-		bool isCurCharVowel;
-		for (int i = 0; i < inputString.size(); i++)
-		{
-			curChar = inputString[i];
-
-			isCurCharVowel = isVowel(curChar);
-
-			if (isCurCharVowel == true)
-			{
-				if (isVowelExist == false)
-					isVowelExist = true;
-
-				contVowelCount++;
-				if (contVowelCount >= 3)
-				{
-					isContCountOver3 = true;
-					break;
-				}
-				contConsonantCount = 0;
-			}
-			else
-			{
-				contConsonantCount++;
-				if (contConsonantCount >= 3)
-				{
-					isContCountOver3 = true;
-					break;
-				}
-				contVowelCount = 0;
-			}
-
-			if (prevChar == curChar)
-			{
-				if(curChar != 'e' && curChar != 'o')
-				{
-					isSameCharCountOver2 = true;
-					break;
-				}
-			}
-
-			prevChar = curChar;
-		}
-
-		if (!isVowelExist || isContCountOver3 || isSameCharCountOver2)
-			cout << "<" << inputString << "> is not acceptable.\n";
-		else
-			cout << "<" << inputString << "> is acceptable.\n";
-	}
-
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//bool isVowel(char inputChar)
+//{
+//	if (inputChar == 'a' ||
+//		inputChar == 'e' ||
+//		inputChar == 'i' ||
+//		inputChar == 'o' ||
+//		inputChar == 'u')
+//		return true;
+//
+//	return false;
+//}
+//
+//int main()
+//{
+//	string inputString;
+//	while (true)
+//	{
+//		cin >> inputString;
+//		if (inputString == "end")
+//			break;
+//
+//		bool isVowelExist = false;
+//		bool isContCountOver3 = false;
+//		bool isSameCharCountOver2 = false;
+//		
+//		char curChar;
+//		char prevChar = '-';
+//		int contVowelCount = 0;
+//		int contConsonantCount = 0;
+//		bool isCurCharVowel;
+//		for (int i = 0; i < inputString.size(); i++)
+//		{
+//			curChar = inputString[i];
+//
+//			isCurCharVowel = isVowel(curChar);
+//
+//			if (isCurCharVowel == true)
+//			{
+//				if (isVowelExist == false)
+//					isVowelExist = true;
+//
+//				contVowelCount++;
+//				if (contVowelCount >= 3)
+//				{
+//					isContCountOver3 = true;
+//					break;
+//				}
+//				contConsonantCount = 0;
+//			}
+//			else
+//			{
+//				contConsonantCount++;
+//				if (contConsonantCount >= 3)
+//				{
+//					isContCountOver3 = true;
+//					break;
+//				}
+//				contVowelCount = 0;
+//			}
+//
+//			if (prevChar == curChar)
+//			{
+//				if(curChar != 'e' && curChar != 'o')
+//				{
+//					isSameCharCountOver2 = true;
+//					break;
+//				}
+//			}
+//
+//			prevChar = curChar;
+//		}
+//
+//		if (!isVowelExist || isContCountOver3 || isSameCharCountOver2)
+//			cout << "<" << inputString << "> is not acceptable.\n";
+//		else
+//			cout << "<" << inputString << "> is acceptable.\n";
+//	}
+//
+//	return 0;
+//}
