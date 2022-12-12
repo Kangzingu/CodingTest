f096adc432eb0d2a12f677ed0bbd7e0f19c7934e
Kangzingu <kanrhaehfdl1@naver.com>
2022-12-12 11:40:25 +0900

    1문제 품
    IF문 좀 대신 써줘

--- CodingTest/삼각형과 세 변.cpp
+++ CodingTest/삼각형과 세 변.cpp
@@ -1,52 +1,52 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-int main()
-{
-	
-	while (true)
-	{
-		vector<int> lens;
-		int len;
-
-		for (int i = 0; i < 3; i++)
-		{
-			cin >> len;
-			lens.push_back(len);
-		}
-
-		if (lens[0] == 0 &&
-			lens[1] == 0 &&
-			lens[2] == 0)
-			break;
-
-		sort(lens.begin(), lens.end());
-
-		if (lens[0] + lens[1] <= lens[2])
-		{
-			cout << "Invalid\n";
-			continue;
-		}
-
-		if (lens[0] == lens[1] &&
-			lens[1] == lens[2])
-		{
-			cout << "Equilateral\n";
-			continue;
-		}
-
-		if (lens[0] == lens[1] ||
-			lens[1] == lens[2] ||
-			lens[2] == lens[0])
-		{
-			cout << "Isosceles\n";
-			continue;
-		}
-
-		cout << "Scalene\n";
-	}
-
-	return 0;
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//int main()
+//{
+//	
+//	while (true)
+//	{
+//		vector<int> lens;
+//		int len;
+//
+//		for (int i = 0; i < 3; i++)
+//		{
+//			cin >> len;
+//			lens.push_back(len);
+//		}
+//
+//		if (lens[0] == 0 &&
+//			lens[1] == 0 &&
+//			lens[2] == 0)
+//			break;
+//
+//		sort(lens.begin(), lens.end());
+//
+//		if (lens[0] + lens[1] <= lens[2])
+//		{
+//			cout << "Invalid\n";
+//			continue;
+//		}
+//
+//		if (lens[0] == lens[1] &&
+//			lens[1] == lens[2])
+//		{
+//			cout << "Equilateral\n";
+//			continue;
+//		}
+//
+//		if (lens[0] == lens[1] ||
+//			lens[1] == lens[2] ||
+//			lens[2] == lens[0])
+//		{
+//			cout << "Isosceles\n";
+//			continue;
+//		}
+//
+//		cout << "Scalene\n";
+//	}
+//
+//	return 0;
+//}
