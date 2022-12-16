21db1fa385bf8bd7d6fe9ae95e13196b4b390d07
Kangzingu <kanrhaehfdl1@naver.com>
2022-12-16 23:06:26 +0900

    1문제 품
    개미굴

---
+++ CodingTest/개미굴.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int numofWholeFood;
+
+struct Cell
+{
+	string key;
+	map<string, Cell> values;
+};
+
+void Print(int layer, Cell curCell, int& printCount)
+{
+	printCount++;
+	if (printCount != 1)
+	{
+		for (int i = 0; i < layer; i++)
+			cout << "--";
+		cout << curCell.key << "\n";
+	}
+
+	for (map<string, Cell>::iterator k = curCell.values.begin(); k != curCell.values.end(); k++)
+		Print(layer + 1, k->second, printCount);
+}
+
+int main()
+{
+	int numofFoodInfo;
+	cin >> numofFoodInfo;
+
+	Cell foods;
+	Cell* curFood;
+	numofWholeFood = 0;
+	for (int i = 0; i < numofFoodInfo; i++)
+	{
+		int numofFood;
+		cin >> numofFood;
+
+		curFood = &foods;
+		for (int j = 0; j < numofFood; j++)
+		{
+			string foodName;
+			cin >> foodName;
+
+			numofWholeFood++;
+			bool isExist = false;
+			for (map<string, Cell>::iterator k = curFood->values.begin(); k != curFood->values.end(); k++)
+			{
+				if (k->first == foodName)
+				{
+					curFood = &(k->second);
+					isExist = true;
+					break;
+				}
+			}
+
+			if (isExist == true)
+				continue;
+
+			Cell newFood;
+			newFood.key = foodName;
+			curFood->values[foodName] = newFood;
+			curFood = &(curFood->values[foodName]);
+		}
+	}
+
+	int printCount = 0;
+	Print(-1, foods, printCount);
+
+	return 0;
+}
--- CodingTest/부분합.cpp
+++ CodingTest/부분합.cpp
@@ -1,48 +1,48 @@
-#include <iostream>
-using namespace std;
-
-#define INF 100001
-struct SubSum
-{
-	int val;
-	int count;
-};
-
-int main()
-{
-	int numofNum, targetMinSumVal;
-	cin >> numofNum >> targetMinSumVal;
-
-	int* nums = new int[numofNum];
-	SubSum subSum;
-	subSum.val = 0;
-	subSum.count = 0;
-	int targetMinSumCount = INF;
-	int frontOffset = 0;
-	for (int i = 0; i < numofNum; i++)
-	{
-		cin >> nums[i];
-		subSum.val += nums[i];
-		subSum.count++;
-
-		while (true)
-		{
-			if (subSum.val < targetMinSumVal)
-				break;
-
-			if (subSum.count < targetMinSumCount)
-				targetMinSumCount = subSum.count;
-
-			subSum.val -= nums[frontOffset];
-			subSum.count--;
-			frontOffset++;
-		}
-	}
-
-	if (targetMinSumCount == INF)
-		cout << 0;
-	else
-		cout << targetMinSumCount;
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//#define INF 100001
+//struct SubSum
+//{
+//	int val;
+//	int count;
+//};
+//
+//int main()
+//{
+//	int numofNum, targetMinSumVal;
+//	cin >> numofNum >> targetMinSumVal;
+//
+//	int* nums = new int[numofNum];
+//	SubSum subSum;
+//	subSum.val = 0;
+//	subSum.count = 0;
+//	int targetMinSumCount = INF;
+//	int frontOffset = 0;
+//	for (int i = 0; i < numofNum; i++)
+//	{
+//		cin >> nums[i];
+//		subSum.val += nums[i];
+//		subSum.count++;
+//
+//		while (true)
+//		{
+//			if (subSum.val < targetMinSumVal)
+//				break;
+//
+//			if (subSum.count < targetMinSumCount)
+//				targetMinSumCount = subSum.count;
+//
+//			subSum.val -= nums[frontOffset];
+//			subSum.count--;
+//			frontOffset++;
+//		}
+//	}
+//
+//	if (targetMinSumCount == INF)
+//		cout << 0;
+//	else
+//		cout << targetMinSumCount;
+//
+//	return 0;
+//}
